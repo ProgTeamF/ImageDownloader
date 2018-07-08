5785b9bbfebe8bf07df4cf2abfcef7e169e96a6f
Nazarii Babii <n.babiy69@gmail.com>
2018-07-08 09:56:24 +0300

    Adds permission for ExternalStorage and fixes realm initializer.

--- app/src/main/java/com/progteamf/test/imagedownloader/activities/MainActivity.java
+++ app/src/main/java/com/progteamf/test/imagedownloader/activities/MainActivity.java
@@ -1,7 +1,12 @@
 package com.progteamf.test.imagedownloader.activities;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Environment;
 import android.support.design.widget.TabLayout;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -12,6 +17,7 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -32,6 +38,8 @@ import com.progteamf.test.imagedownloader.model.SortType;
 import com.progteamf.test.imagedownloader.model.Status;
 import com.progteamf.test.imagedownloader.utils.ItemDivider;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.GregorianCalendar;
 import java.util.List;
@@ -45,6 +53,9 @@ import io.realm.RealmConfiguration;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static boolean REALM_ISNT_INIT = true;
+    private static final String MAIN_TAG = "MainActivity_log";
+
     private SectionsPagerAdapter mSectionsPagerAdapter;
     private ViewPager mViewPager;
 
@@ -52,6 +63,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.e("MAIN", "onCreate");
+
+        while (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            getPermissionForExternalStorage();
+        }
+        if (REALM_ISNT_INIT) {
+            try {
+                initRealm();
+            } catch (IOException e) {
+                Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_LONG);
+            } catch (IllegalArgumentException e) {
+                Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_LONG);
+            }
+        }
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -68,207 +93,238 @@ public class MainActivity extends AppCompatActivity {
         mViewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
         tabLayout.addOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(mViewPager));
 
-        //initialization of Realm DB
-        initRealm();
+
     }
 
-    private void initRealm() {
+
+    private void initRealm() throws IOException {
+        REALM_ISNT_INIT = false;
+        Log.e(MAIN_TAG, "realm initialization.");
         Realm.init(this);
         RealmConfiguration realmConfig = new RealmConfiguration.Builder()
+                .directory(new File(Environment.getExternalStorageDirectory() + "/BIGDIG/test/realm"))
                 .name("progTeamF.realm")
                 .schemaVersion(0)
                 .build();
         Realm.setDefaultConfiguration(realmConfig);
     }
 
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public static class PlaceholderFragment extends Fragment {
+
+    private static final String LINK_TAG = "link_tag";
+    private static final String APP_A_URL_TAG = "app_a_url_tag";
+
     /**
-     * A placeholder fragment containing a simple view.
+     * attributes for displaying tab of history
      */
-    public static class PlaceholderFragment extends Fragment {
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private LinearLayoutManager linearLayoutManager;
+    private List<Image> images = new ArrayList<>();
 
-        private static final String LINK_TAG = "link_tag";
-        private static final String APP_A_URL_TAG = "app_a_url_tag";
+    public List<Image> getImages() {
+        return images;
+    }
 
-        /**
-         * attributes for displaying tab of history
-         */
-        private RecyclerView mRecyclerView;
-        private RecyclerView.Adapter mAdapter;
-        private RecyclerView.LayoutManager mLayoutManager;
-        private LinearLayoutManager linearLayoutManager;
-        private List<Image> images = new ArrayList<>();
+    public void setImages(List<Image> images) {
+        this.images = images;
+    }
 
-        public List<Image> getImages() {
-            return images;
+    /**
+     * The fragment argument representing the section number for this
+     * fragment.
+     */
+    private static final String ARG_SECTION_NUMBER = "section_number";
+
+    public PlaceholderFragment() {
+    }
+
+    /**
+     * Returns a new instance of this fragment for the given section
+     * number.
+     */
+    public static PlaceholderFragment newInstance(int sectionNumber) {
+        PlaceholderFragment fragment = new PlaceholderFragment();
+        Bundle args = new Bundle();
+        args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.menu_main, menu);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        switch (id) {
+            case R.id.action_sort_by_date:
+                try {
+                    images = SortingImageController.sort(images, SortType.SORT_BY_DATE);
+                } catch (SortException e) {
+                    Toast.makeText(getActivity(), e.getMessage(), Toast.LENGTH_SHORT);
+                }
+                break;
+            case R.id.action_sort_by_status:
+                try {
+                    images = SortingImageController.sort(images, SortType.SORT_BY_STATUS);
+                } catch (SortException e) {
+                    Toast.makeText(getActivity(), e.getMessage(), Toast.LENGTH_SHORT);
+                }
+                break;
         }
 
-        public void setImages(List<Image> images) {
-            this.images = images;
-        }
+        mAdapter = new HistoryAdapter(getContext(), images);
+        mRecyclerView.setAdapter(mAdapter);
 
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
+        return super.onOptionsItemSelected(item);
+    }
 
-        public PlaceholderFragment() {
-        }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        if (getArguments().getInt(ARG_SECTION_NUMBER) == 0) {
 
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
 
-        @Override
-        public void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
+            View view = inflater.inflate(R.layout.fragment_test, null);
+            setHasOptionsMenu(false);
+
+            final EditText linkEdit = view.findViewById(R.id.link);
+            Button ok = view.findViewById(R.id.ok_link);
+            final Intent openAppB = getContext().getPackageManager()
+                    .getLaunchIntentForPackage("com.progteamf.test.imageviewer");
+            ok.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View view) {
+                    if (openAppB != null) {
+                        openAppB.addCategory(APP_A_URL_TAG);
+                        openAppB.putExtra(LINK_TAG, linkEdit.getText().toString().trim());
+                        startActivity(openAppB);
+                    } else {
+                        Toast.makeText(getContext(), "NULL", Toast.LENGTH_LONG).show();
+                    }
+                }
+            });
+
+            return view;
+
+
+        } else if (getArguments().getInt(ARG_SECTION_NUMBER) == 1) {
+
+
+            View rootView = inflater.inflate(R.layout.fragment_history, container, false);
             setHasOptionsMenu(true);
-        }
 
-        @Override
-        public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
-            inflater.inflate(R.menu.menu_main, menu);
-            super.onCreateOptionsMenu(menu, inflater);
-        }
+            images.add(new Image("", "http://www.google.com", Status.ERROR, new GregorianCalendar(2018, 06, 5, 16, 30)));
+            images.add(new Image("", "http://www.telegram.org", Status.UNKNOWN, new GregorianCalendar(2018, 06, 6, 16, 30)));
+            images.add(new Image("", "http://www.vk.com", Status.UNKNOWN, new GregorianCalendar(2018, 06, 10, 16, 30)));
+            images.add(new Image("", "http://www.instagram.com", Status.DOWNLOADED, new GregorianCalendar(2018, 06, 6, 14, 30)));
+            images.add(new Image("", "http://www.facebook.com", Status.DOWNLOADED, new GregorianCalendar(2018, 06, 5, 16, 31)));
 
-        @Override
-        public boolean onOptionsItemSelected(MenuItem item) {
-            // Handle action bar item clicks here. The action bar will
-            // automatically handle clicks on the Home/Up button, so long
-            // as you specify a parent activity in AndroidManifest.xml.
-            int id = item.getItemId();
+            images.add(new Image("", "http://www.telegram.org", Status.UNKNOWN, new GregorianCalendar(2018, 06, 6, 16, 30)));
+            images.add(new Image("", "http://www.vk.com", Status.UNKNOWN, new GregorianCalendar(2018, 06, 10, 16, 30)));
+            images.add(new Image("", "http://www.instagram.com", Status.DOWNLOADED, new GregorianCalendar(2018, 06, 6, 14, 30)));
+            images.add(new Image("", "http://www.facebook.com", Status.DOWNLOADED, new GregorianCalendar(2018, 06, 5, 16, 31)));
 
-            switch (id) {
-                case R.id.action_sort_by_date:
-                    try {
-                        images = SortingImageController.sort(images, SortType.SORT_BY_DATE);
-                    } catch (SortException e) {
-                        Toast.makeText(getActivity(), e.getMessage(), Toast.LENGTH_SHORT);
-                    }
-                    break;
-                case R.id.action_sort_by_status:
-                    try {
-                        images = SortingImageController.sort(images, SortType.SORT_BY_STATUS);
-                    } catch (SortException e) {
-                        Toast.makeText(getActivity(), e.getMessage(), Toast.LENGTH_SHORT);
-                    }
-                    break;
-            }
+            /** There is initialization of recyclerView which displays links*/
+            mRecyclerView = (RecyclerView) rootView.findViewById(R.id.history_recycler);
 
+            // use this setting to improve performance because changes
+            // in content do not change the layout size of the RecyclerView
+            mRecyclerView.setHasFixedSize(true);
+
+            // add divider between lines
+            mRecyclerView.addItemDecoration(new ItemDivider(getActivity().getApplicationContext()));
+
+            // use a linear layout manager
+            linearLayoutManager = new LinearLayoutManager(getActivity().getApplicationContext());
+            mLayoutManager = linearLayoutManager;
+            mRecyclerView.setLayoutManager(mLayoutManager);
+
+            // specify an adapter
             mAdapter = new HistoryAdapter(getContext(), images);
+
+            //setting adapter
             mRecyclerView.setAdapter(mAdapter);
+            return rootView;
+        } else return null;
+    }
+}
 
-            return super.onOptionsItemSelected(item);
-        }
+public class SectionsPagerAdapter extends FragmentPagerAdapter {
 
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            if (getArguments().getInt(ARG_SECTION_NUMBER) == 0) {
-
-
-                View view = inflater.inflate(R.layout.fragment_test, null);
-                setHasOptionsMenu(false);
-
-                final EditText linkEdit = view.findViewById(R.id.link);
-                Button ok = view.findViewById(R.id.ok_link);
-                final Intent openAppB = getContext().getPackageManager()
-                        .getLaunchIntentForPackage("com.progteamf.test.imageviewer");
-                ok.setOnClickListener(new View.OnClickListener() {
-                    public void onClick(View view) {
-                        if (openAppB != null) {
-                            openAppB.addCategory(APP_A_URL_TAG);
-                            openAppB.putExtra(LINK_TAG, linkEdit.getText().toString().trim());
-                            startActivity(openAppB);
-                        } else {
-                            Toast.makeText(getContext(), "NULL", Toast.LENGTH_LONG).show();
-                        }
-                    }
-                });
-
-                return view;
-
-
-            } else if (getArguments().getInt(ARG_SECTION_NUMBER) == 1) {
-
-
-                View rootView = inflater.inflate(R.layout.fragment_history, container, false);
-                setHasOptionsMenu(true);
-
-                images.add(new Image("", "http://www.google.com", Status.ERROR, new GregorianCalendar(2018, 06, 5, 16, 30)));
-                images.add(new Image("", "http://www.telegram.org", Status.UNKNOWN, new GregorianCalendar(2018, 06, 6, 16, 30)));
-                images.add(new Image("", "http://www.vk.com", Status.UNKNOWN, new GregorianCalendar(2018, 06, 10, 16, 30)));
-                images.add(new Image("", "http://www.instagram.com", Status.DOWNLOADED, new GregorianCalendar(2018, 06, 6, 14, 30)));
-                images.add(new Image("", "http://www.facebook.com", Status.DOWNLOADED, new GregorianCalendar(2018, 06, 5, 16, 31)));
-
-                images.add(new Image("", "http://www.telegram.org", Status.UNKNOWN, new GregorianCalendar(2018, 06, 6, 16, 30)));
-                images.add(new Image("", "http://www.vk.com", Status.UNKNOWN, new GregorianCalendar(2018, 06, 10, 16, 30)));
-                images.add(new Image("", "http://www.instagram.com", Status.DOWNLOADED, new GregorianCalendar(2018, 06, 6, 14, 30)));
-                images.add(new Image("", "http://www.facebook.com", Status.DOWNLOADED, new GregorianCalendar(2018, 06, 5, 16, 31)));
-
-                /** There is initialization of recyclerView which displays links*/
-                mRecyclerView = (RecyclerView) rootView.findViewById(R.id.history_recycler);
-
-                // use this setting to improve performance because changes
-                // in content do not change the layout size of the RecyclerView
-                mRecyclerView.setHasFixedSize(true);
-
-                // add divider between lines
-                mRecyclerView.addItemDecoration(new ItemDivider(getActivity().getApplicationContext()));
-
-                // use a linear layout manager
-                linearLayoutManager = new LinearLayoutManager(getActivity().getApplicationContext());
-                mLayoutManager = linearLayoutManager;
-                mRecyclerView.setLayoutManager(mLayoutManager);
-
-                // specify an adapter
-                mAdapter = new HistoryAdapter(getContext(), images);
-
-                //setting adapter
-                mRecyclerView.setAdapter(mAdapter);
-                return rootView;
-            } else return null;
-        }
+    public SectionsPagerAdapter(FragmentManager fm) {
+        super(fm);
     }
 
-    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+    @Override
+    public Fragment getItem(int position) {
+        // getItem is called to instantiate the fragment for the given page.
+        // Return a PlaceholderFragment (defined as a static inner class below).
+        return PlaceholderFragment.newInstance(position);
+    }
 
-        public SectionsPagerAdapter(FragmentManager fm) {
-            super(fm);
+    @Override
+    public int getCount() {
+        // Show 2 total pages.
+        return 2;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch (position) {
+            case 0:
+                return "TEST";
+            case 1:
+                return "HISTORY";
         }
+        return null;
+    }
 
-        @Override
-        public Fragment getItem(int position) {
-            // getItem is called to instantiate the fragment for the given page.
-            // Return a PlaceholderFragment (defined as a static inner class below).
-            return PlaceholderFragment.newInstance(position);
-        }
+}
 
-        @Override
-        public int getCount() {
-            // Show 2 total pages.
-            return 2;
-        }
 
-        @Override
-        public CharSequence getPageTitle(int position) {
-            switch (position) {
-                case 0:
-                    return "TEST";
-                case 1:
-                    return "HISTORY";
+    private void getPermissionForExternalStorage() {
+        do {
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+
+
+                // You can show your dialog message here but instead I am
+                // showing the grant permission dialog box
+                ActivityCompat.requestPermissions(this, new String[]{
+                                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                                Manifest.permission.READ_EXTERNAL_STORAGE},
+                        10);
+
+
+            } else {
+
+                //Requesting permission
+                ActivityCompat.requestPermissions(this, new String[]{
+                                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                                Manifest.permission.READ_EXTERNAL_STORAGE},
+                        10);
+
             }
-            return null;
         }
+        while (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED);
     }
-
 
 }
